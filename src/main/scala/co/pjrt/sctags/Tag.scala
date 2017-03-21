aa9f91627eb3ef1a03d9fb904bb7c809f33ced64
Pedro Rodriguez <prodrigueztavarez@imprivata.com>
2017-03-21 18:00:15 -0400

    Implement tagline generation for vim
    Implements a new `vimTagLine` function in `Tag` that creates a
    Vim-compatible tag line.
    
    See http://vimdoc.sourceforge.net/htmldoc/tagsrch.html#tags-file-format

--- src/main/scala/co/pjrt/sctags/Tag.scala
+++ src/main/scala/co/pjrt/sctags/Tag.scala
@@ -6,11 +6,65 @@ case class Tag(
     prefix: Option[String],
     basicName: String,
     mods: Seq[Mod],
-    pos: TagPosition
-) {
+    pos: TagPosition) {
 
   final val tagName: String =
     prefix.fold(basicName)(_ + "." + basicName)
+
+  private final val tagAddress =
+    pos.line + "G" + pos.col + "|"
+
+  private final val term = ";\""
+  private final val tab = "\t"
+
+  private def extras(fields: Seq[(String, String)]) =
+    term + tab + fields.map(t => t._1 + ":" + t._2).mkString(tab)
+
+  /**
+   * Visibility of the tag. This means either:
+   *
+   * * Private: Only seen in this file/object/class/trait
+   * * PrivateIsh: Can be seen from more than just this file
+   * * Public: Everything else
+   *
+   * Note that Protected also means Public. This visibility is only meant to
+   * determine whether we create a static tag or not.
+   *
+   * This is not a complete implementation. We could technially do something
+   * with protected[thisPackage] too.
+   */
+  lazy val visibility: Visibility =
+    mods.collect {
+      case Mod.Private(Name.Anonymous()) => Private
+      case Mod.Private(Name.Indeterminate(name)) =>
+        // DESNOTE(2017-03-21, prodriguez): If the name of the private thing
+        // is the parent object, then it is just private.
+        if (prefix.contains(name))
+          Private
+        else
+          PrivateIsh
+    }.headOption.getOrElse(Public)
+
+  /**
+   * Whether this is a static tag as defined in C
+   */
+  lazy val isStaticTag: Boolean =
+    visibility == Private
+
+  /**
+   * Given a [[Tag]] and a file name, create a vim tag line
+   *
+   * See http://vimdoc.sourceforge.net/htmldoc/tagsrch.html#tags-file-format
+   */
+  def vimTagLine(fileName: String): String = {
+    val static =
+      if (isStaticTag) Seq(("file" -> fileName))
+      else Seq.empty
+
+    val langTag = "language" -> "scala"
+    val fields = static :+ langTag
+    List(tagName, fileName, tagAddress).mkString(tab) + extras(fields)
+  }
 
   override def toString: String =
     s"Tag($tagName, $mods, ${pos.line}, ${pos.col})"
@@ -43,7 +97,21 @@ object Tag {
     Tag(prefix, basicName, mods, TagPosition.fromPosition(pos))
   }
 
+  implicit val ordering: Ordering[Tag] =
+    Ordering.by(_.tagName)
+
 }
+
+/**
+ * Visibility of a tag
+ *
+ * At the end of the day, we only care about whether we should make a static
+ * tag or a non-static tag (private vs public). This is probably overkill.
+ */
+sealed trait Visibility
+case object Private extends Visibility
+case object PrivateIsh extends Visibility
+case object Public extends Visibility
 
 case class TagPosition(line: Int, col: Int)
 
---
+++ src/test/scala/co/pjrt/sctags/TagTest.scala
@@ -0,0 +1,83 @@
+package co.pjrt.sctags
+
+import scala.meta._
+
+import org.scalatest.{Assertion, FreeSpec, Matchers}
+
+import Utils._
+
+class TagTest extends FreeSpec with Matchers {
+
+  import Mod._
+  import Name._
+
+  def priv(scope: Option[String]) =
+    scope match {
+      case None => Private(Anonymous())
+      case Some(n) => Private(Indeterminate(n))
+    }
+
+  def prod(scope: Option[String]) =
+    scope match {
+      case None => Protected(Anonymous())
+      case Some(n) => Protected(Indeterminate(n))
+    }
+
+  "Static tags" - {
+
+    "Are tags that are private" in {
+      val mods = Seq(
+        priv(None)
+      )
+      val t = Tag(Some("Obj"), "tagName", mods, TagPosition(0,0))
+      t.isStaticTag shouldBe true
+    }
+
+    "Are tags that are private and have the scope as the parent" in {
+      val mods = Seq(
+        priv(Some("Obj"))
+      )
+      val t = Tag(Some("Obj"), "tagName", mods, TagPosition(0,0))
+      t.isStaticTag shouldBe true
+    }
+
+    "Are NOT private that don't have the scope as the parent" in {
+      val mods = Seq(
+        priv(Some("test"))
+      )
+      val t = Tag(Some("Obj"), "tagName", mods, TagPosition(0,0))
+      t.isStaticTag shouldBe false
+    }
+
+    "Are NOT any other visibility" in {
+      val prodMods1 = Seq(prod(None))
+      val prodMods2 = Seq(prod(Some("Obj")))
+      val publicMods = Seq.empty[Mod]
+
+      val t1 = Tag(Some("Obj"), "tagName", prodMods1, TagPosition(0,0))
+      val t2 = Tag(Some("Obj"), "tagName", prodMods2, TagPosition(0,0))
+      val t3 = Tag(Some("Obj"), "tagName", publicMods, TagPosition(0,0))
+      t1.isStaticTag shouldBe false
+      t2.isStaticTag shouldBe false
+      t3.isStaticTag shouldBe false
+    }
+  }
+
+  "Vim Tag line" - {
+    "should produce a complete non-static tag line" in {
+      val t = Tag(Some("Obj"), "tagName", Nil, TagPosition(0, 1))
+
+      val testFile = "TestFile.scala"
+      t.vimTagLine(testFile) shouldBe
+        s"""Obj.tagName\t$testFile\t0G1|;"\tlanguage:scala"""
+    }
+
+    "should produce a complete static tag line" in {
+      val t = Tag(Some("Obj"), "tagName", Seq(priv(None)), TagPosition(0, 1))
+
+      val testFile = "TestFile.scala"
+      t.vimTagLine(testFile) shouldBe
+        s"""Obj.tagName\t$testFile\t0G1|;"\tfile:$testFile\tlanguage:scala"""
+    }
+  }
+}
